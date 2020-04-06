1bb31437f8572f8878d57fbdc1ce37b99a868a05
Jakub Zaremba <jzaremba@MAgiC.local>
2020-04-06 20:45:53 +0200

    New movment engine + gravity Also clean up asset window + create new player asset

---
+++ Assets/scripts/Movment.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movment : MonoBehaviour
+{
+    public Rigidbody playerRB;
+    public float speed = 200f;
+    void Update()
+    {
+        if (Input.GetKey("w"))
+            playerRB.AddForce(0, 0, speed * Time.deltaTime);
+        if (Input.GetKey("s"))
+            playerRB.AddForce(0, 0, -speed * Time.deltaTime);
+        if (Input.GetKey("a"))
+            playerRB.AddForce(speed * -Time.deltaTime, 0, 0);
+        if (Input.GetKey("d"))
+            playerRB.AddForce(speed * Time.deltaTime, 0, 0);
+    }
+}
