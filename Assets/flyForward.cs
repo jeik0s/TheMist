ba2b3ddc652930b7dc8dde683bc89c3d590d4f26
Jakub Zaremba <jzaremba@MAgiC.local>
2020-06-22 19:49:09 +0200

    shooting in pajonk form

---
+++ Assets/flyForward.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flyForward : MonoBehaviour
+{
+    public Rigidbody rigidbody;
+    public float shootforce;
+    public GameObject SpiderTransform;
+    Vector3 direction;
+
+    //Vector3 direction = (SpiderTransform.position + SpiderTransform.forward) - SpiderTransform.position;
+    //rigidbody.AddForce(direction* pushForce);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpiderTransform = GameObject.Find("Spider_Fuga_Blue_Green");
+        direction = (SpiderTransform.transform.position + SpiderTransform.transform.forward) - SpiderTransform.transform.position;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "enemy")
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rigidbody.AddForce(direction * -shootforce);
+
+
+
+
+        Destroy(gameObject, 1);
+    }
+}
---
+++ Assets/scripts/shootWeb.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shootWeb : MonoBehaviour
+{
+
+    float timer = 0.0f;
+    public GameObject bulletPrefab;
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (Input.GetKey("space") && timer > 2)
+        {
+            timer = 0;
+            GameObject bulletObject = Instantiate(bulletPrefab);
+            bulletObject.transform.position = transform.position;
+        }
+    }
+}
